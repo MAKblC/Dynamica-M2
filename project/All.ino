ecb5efe55c595a8e02c3075fbae7d96f7bd1e27a
Max <41009925+MAKblC@users.noreply.github.com>
2024-10-28 15:05:52 +0300

    Update All.ino

--- project/All.ino
+++ project/All.ino
@@ -4,6 +4,13 @@ Adafruit_PWMServoDriver mdyn2 = Adafruit_PWMServoDriver(0x79);  // адрес п
 
 #include "MGS_CLMLN8.h"
 
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+MPU6050 mpu;
+uint8_t fifoBuffer[45];  // буфер
+int turnSpeed = 15;      // скорость поворота
+int compensation = 2;    // компенсация поворота (зависит от скорости поворота)
+
 #include <VL53L0X.h>  // библиотека для датчика MGS-D20
 VL53L0X loxL;
 VL53L0X loxR;
@@ -13,6 +20,7 @@ VL53L0X loxR;
 MGB_I2C63 mgb_i2c63 = MGB_I2C63(false);
 #define DISTL 0x05
 #define DISTR 0x03
+#define GYRO 0x07
 
 #include "Adafruit_APDS9960.h"
 Adafruit_APDS9960 apds9960;
@@ -56,6 +64,11 @@ void setup() {
   }
   // Инициализация режимов работы датчика
   apds9960.enableColor(true);
+
+  mgb_i2c63.setBusChannel(GYRO);
+  mpu.initialize();
+  mpu.dmpInitialize();
+  mpu.setDMPEnabled(true);
 }
 
 void loop() {
@@ -108,7 +121,8 @@ void loop() {
       delay(50);
       mdyn2.motor_setpower(2, 0, false);
       mdyn2.motor_setpower(1, 0, true);
-      while (true) {};  // стоим
+      angle(true, 180);  // повернуть против часовой на 180
+      while (true) {};   // стоим
     }
   }
 }
@@ -200,5 +214,72 @@ String getColor() {
       mdyn2.rgb_set(i, 0, 0, 255);
     }
     return "BLUE";
+  }
+}
+
+void angle(bool side, int angle) {
+  // измеряем текущий угол
+  int currentAngle = angleMeasure();
+  // если нужно повернуть по часовой (направо)
+  if (side == false) {
+    // берем остаток от круга и поворачиваем, пока разница не станет меньше 10, но больше 0
+    while (((currentAngle + angle) % 360) - angleMeasure() > compensation or ((currentAngle + angle) % 360) - angleMeasure() < 0) {
+      // поворот на одном колесе
+      mdyn2.motor_setpower(1, turnSpeed, side);  // расскомментировать чтоб поворачивать вокруг оси
+      mdyn2.motor_setpower(2, turnSpeed, side);
+    }
+    // другой алгоритм поворота по часовой
+    /*if (currentAngle + angle < 360) {
+      while (currentAngle + angle - angleMeasure() > 10) {
+        motorA_setpower(5, side);
+        motorB_setpower(5, side);
+      }
+    } else {
+      while (angle - (360 - currentAngle) - angleMeasure() > 10 or angle - (360 - currentAngle) - angleMeasure() < 0) {
+        motorA_setpower(5, side);
+        motorB_setpower(5, side);
+      }
+    }*/
+  } else {
+    // если поворачиваем налево (против часовой)
+    // в простой ситуации, когда нужно повернуться на угол меньший, чем текущий
+    if (currentAngle > angle) {
+      // поворачиваем пока разница между углами не станет меньше -10
+      while (currentAngle - angle - angleMeasure() < -(compensation)) {
+        // поворот на одном колесе
+        mdyn2.motor_setpower(1, turnSpeed, side);  // расскомментировать чтоб поворачивать вокруг оси
+        mdyn2.motor_setpower(2, turnSpeed, side);
+      }
+      // в сложной ситуации, когда нужно осилить переход через 0
+    } else {
+      while (360 - (angle - currentAngle) - angleMeasure() > 0 or 360 - (angle - currentAngle) - angleMeasure() < -(compensation)) {
+        // поворот на одном колесе
+        mdyn2.motor_setpower(1, turnSpeed, side);  // расскомментировать чтоб поворачивать вокруг оси
+        mdyn2.motor_setpower(2, turnSpeed, side);
+      }
+    }
+  }
+  // резко тормозим с включением на мгновение обратных моторов
+  mdyn2.motor_setpower(1, turnSpeed, !side);
+  mdyn2.motor_setpower(2, turnSpeed, !side);
+  delay(50);
+  mdyn2.motor_setpower(1, 0, side);
+  mdyn2.motor_setpower(2, 0, side);
+}
+
+// измерение угла в градусах от 0 до 360
+int angleMeasure(void) {
+  mgb_i2c63.setBusChannel(GYRO);
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
+    // переменные для расчёта (ypr можно вынести в глобальеы переменные)
+    Quaternion q;
+    VectorFloat gravity;
+    float ypr[3];
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    int angle = map(degrees(ypr[0]), -180, 180, 0, 360);
+    Serial.println(angle);
+    return angle;
   }
 }
